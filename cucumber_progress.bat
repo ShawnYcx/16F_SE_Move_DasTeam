69616a3a430c25493d7d5f366f1613bea4f11c86
STEVEN-PRIVATE\steven <SXQ13A@ACU.EDU>
2016-10-26 15:03:43 -0500

    add commitment

---
+++ cucumber_progress.bat
@@ -0,0 +1,5 @@
+javac -cp "jars/*;." step_definitions/MoveClassTest.java 
+                     MySQL/MySQLAccess.java 
+                     implementation/MoveClass.java
+                     
+java -cp "jars/*;." cucumber.api.cli.Main -p progress --snippets camelcase features -g step_definitions features
--- go_windows.bat
+++
@@ -1,4 +0,0 @@
-::CS374 DasTeam: Shawn scy12a, Steven sxq13a, Ivan ioa13a.
-javac -cp "jars/*;." step_definitions/main.java implementation/GetStudents.java MySQL/MySQLAccess.java
-
-java -cp "jars/*;." step_definitions/main
